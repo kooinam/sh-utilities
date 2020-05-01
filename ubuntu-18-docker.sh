3b34d697d300db7fe68b730004191fabd72971ad
kooi nam ng <kooinamng@koois-MacBook-Pro.local>
2020-05-01 22:56:18 +0800

    .

--- ubuntu-18-docker.sh
+++ ubuntu-18-docker.sh
@@ -4,4 +4,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt install --assume-yes docker-ce
